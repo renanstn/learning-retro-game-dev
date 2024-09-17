f75fb818e29dd7ada23267568822e7aaae5ea436
Renan Santana <renan_ota@hotmail.com>
2024-09-17 18:37:41 -0300

    Update generate_sprites_and_compile.sh

--- GameBoy/berg/generate_sprites_and_compile.sh
+++ GameBoy/berg/generate_sprites_and_compile.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Trata sprites, converte-os, reduz a paleta de cores, redimensiona, etc
+# Em seguida compila o jogo.
+# Os sprites compilados são incluidos no código fonte com o comando INCBIN.
+
 convert berg-pixel.png -colors 4 -brightness-contrast 10x70 berg-4-colors.png && \
 convert berg-4-colors.png -background white -gravity NorthWest -extent 256x256 berg-screen.png && \
 rgbgfx -u berg-screen.png -o berg.2bpp -t berg.tilemap && \
