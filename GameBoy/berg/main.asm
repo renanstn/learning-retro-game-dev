920cc4aa1df3f68ef30bd4bb2e7b115a0cd6ac72
Renan Santana <renan_ota@hotmail.com>
2024-09-18 08:53:52 -0300

    Update main.asm

--- GameBoy/berg/main.asm
+++ GameBoy/berg/main.asm
@@ -122,6 +122,26 @@ Memcopy:
     jp nz, Memcopy
     ret
 
+; Draw a text on screen
+; @param de: Start tile
+; @param hl: Text address
+DrawTextTilesLoop:
+    ; Check for the end of string character 255
+    ld a, [hl]
+    cp 255 ; Keep writing the string until find value 255.
+    ret z
+
+    ; Write the current character (in hl) to the address
+    ; on the tilemap (in de)
+    ld a, [hl]
+    ld [de], a
+
+    inc hl
+    inc de
+
+    ; move to the next character and next background tile
+    jp DrawTextTilesLoop
+
 letters: INCBIN "letters.2bpp"
 lettersEnd:
 
