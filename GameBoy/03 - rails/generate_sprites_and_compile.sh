3fbb0a2809608b523bfc15ea7e0e546ca2e00424
Renan <renan_ota@hotmail.com>
2024-09-23 22:24:14 -0300

    Progress...

--- GameBoy/03 - rails/generate_sprites_and_compile.sh
+++ GameBoy/03 - rails/generate_sprites_and_compile.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# Trata sprites, converte-os, reduz a paleta de cores, redimensiona, etc
-# Em seguida compila o jogo.
-# Os sprites compilados são incluidos no código fonte com o comando INCBIN.
+# Convert font
+rgbgfx assets/text-font.png -u -c "#FFFFFF,#cbcbcb,#414141,#000000;" -o text-font.2bpp && \
 
-convert berg-pixel.png -colors 4 -brightness-contrast 10x70 berg-4-colors.png && \
-convert berg-4-colors.png -background white -gravity NorthWest -extent 256x256 berg-screen.png && \
-rgbgfx -u berg-screen.png -o berg.2bpp -t berg.tilemap && \
+# Convert spaceship sprite
+rgbgfx assets/spaceship.png -u -o spaceship.2bpp && \
+
+# Convert background sprite
+rgbgfx assets/background.png -u -c "#9bbc0f,#8bac0f,#306230,#0f380f;" -o background.2bpp -t background.tilemap && \
+
+# Compile game
 rgbasm -o main.o main.asm && \
 rgblink -o rails.gb main.o && \
-rgbfix -v -p 0xFF rails.gb
+rgbfix -v -p 0xFF rails.gb && \
+rgblink -n rails.sym main.o && \
 
+# Clean trash
+rm main.o
+
+# Copy rom to windows folder
 if [ "$1" == "--win" ]; then
     cp rails.gb /mnt/c/Users/renan/Downloads/
+    cp rails.sym /mnt/c/Users/renan/Downloads/
 fi
+
+echo "Done!"
--- GameBoy/03 - rails/main.asm
+++ GameBoy/03 - rails/main.asm
@@ -17,15 +17,27 @@ WaitVBlank: ; Wait for VBlank -------------------------------------------------
 
     ; Copy sprites to VRAM ----------------------------------------------------
     ; Copy letters tiles
-    ld de, Letters
-    ld hl, $9000
-    ld bc, LettersEnd - Letters
+    ; ld de, Letters
+    ; ld hl, _VRAM9000
+    ; ld bc, LettersEnd - Letters
+    ; call Memcopy
+
+    ; Copy background tiles
+    ld de, Background
+    ld hl, _VRAM9000
+    ld bc, BackgroundEnd - Background
     call Memcopy
 
-    ; Copy player tiles
-    ld de, Player
-    ld hl, $9340
-    ld bc, PlayerEnd - Player
+    ; Copy spaceship tiles
+    ld de, Spaceship
+    ld hl, _VRAM8000
+    ld bc, SpaceshipEnd - Spaceship
+    call Memcopy
+
+    ; Copy background tilemap
+    ld de, BackgroundTilemap
+    ld hl, _SCRN0
+    ld bc, BackgroundTilemapEnd - BackgroundTilemap
     call Memcopy
 
     ; Clear Object attribute memory (OAM) -------------------------------------
@@ -37,14 +49,43 @@ ClearOam:
     dec b
     jp nz, ClearOam
 
-    ; Initialize the player sprite in OAM -------------------------------------
+    ; Initialize the player sprite in OAM (4 parts) ---------------------------
+    ; Up left
     ld hl, _OAMRAM
-    ld a, 128 + 16
+    ld a, 40 + 16
     ld [hli], a
     ld a, 16 + 8
     ld [hli], a
+    ld a, $0
+    ld [hli], a
     ld a, 0
     ld [hli], a
+    ; Up right
+    ld a, 40 + 16
+    ld [hli], a
+    ld a, 24 + 8
+    ld [hli], a
+    ld a, $1
+    ld [hli], a
+    ld a, 0
+    ld [hli], a
+    ; Down left
+    ld a, 48 + 16
+    ld [hli], a
+    ld a, 16 + 8
+    ld [hli], a
+    ld a, $2
+    ld [hli], a
+    ld a, 0
+    ld [hli], a
+    ; Down right
+    ld a, 48 + 16
+    ld [hli], a
+    ld a, 24 + 8
+    ld [hli], a
+    ld a, $3
+    ld [hli], a
+    ld a, 0
     ld [hli], a
 
     ; Turn the LCD and objects on ---------------------------------------------
@@ -57,9 +98,65 @@ ClearOam:
     ld a, %11100100
     ld [rOBP0], a
 
-; Game loop -------------------------------------------------------------------
-Done:
-    jp Done
+    ; Initialize global variables ---------------------------------------------
+    ld a, 0
+    ld [wCurKeys], a
+    ld [wNewKeys], a
+
+; Game loop ===================================================================
+Main:
+    ; Wait until it's *not* VBlank
+    ld a, [rLY]
+    cp 144
+    jp nc, Main
+WaitVBlank2:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank2
+
+    call UpdateKeys
+
+; First, check if the left button is pressed
+CheckLeft:
+    ld a, [wCurKeys]
+    and a, PADF_LEFT
+    jp z, CheckRight
+Left:
+    ; Move the player one pixel to the left
+    ld a, [_OAMRAM + 1]
+    dec a
+    ld [_OAMRAM + 1], a
+    ld a, [_OAMRAM + 5]
+    dec a
+    ld [_OAMRAM + 5], a
+    ld a, [_OAMRAM + 9]
+    dec a
+    ld [_OAMRAM + 9], a
+    ld a, [_OAMRAM + 13]
+    dec a
+    ld [_OAMRAM + 13], a
+    jp Main
+
+; Now check the if the right button is pressed
+CheckRight:
+    ld a, [wCurKeys]
+    and a, PADF_RIGHT
+    jp z, Main
+Right:
+    ; Move the player one pixel to the right
+    ld a, [_OAMRAM + 1]
+    inc a
+    ld [_OAMRAM + 1], a
+    ld a, [_OAMRAM + 5]
+    inc a
+    ld [_OAMRAM + 5], a
+    ld a, [_OAMRAM + 9]
+    inc a
+    ld [_OAMRAM + 9], a
+    ld a, [_OAMRAM + 13]
+    inc a
+    ld [_OAMRAM + 13], a
+    jp Main
 
 ; =============================================================================
 
@@ -94,18 +191,60 @@ DrawTextTilesLoop:
     ; move to the next character and next background tile
     jp DrawTextTilesLoop
 
-; =============================================================================
+; Read player input
+UpdateKeys:
+    ; Poll half the controller (buttons A, B, start, select)
+    ld a, P1F_GET_BTN
+    call .onenibble
+    ld b, a     ; B7-4 = 1; B3-0 = unpressed buttons
 
-Letters: INCBIN "letters.2bpp"
+    ; Poll the other half (directions buttons)
+    ld a, P1F_GET_DPAD
+    call .onenibble
+    swap a      ; A3-0 = unpressed directions; A7-4 = 1
+    xor a, b    ; A = pressed buttons + directions
+    ld b, a     ; B = pressed buttons + directions
+
+    ; And release the controller
+    ld a, P1F_GET_NONE
+    ldh [rP1], a
+
+    ; Combine with previous wCurrKeys to make wNewKeys
+    ld a, [wCurKeys]
+    xor a, b    ; A = keys that changed state
+    and a, b    ; A = keys that changed to pressed
+    ld [wNewKeys], a
+    ld a, b
+    ld [wCurKeys], a
+    ret
+
+.onenibble
+    ldh [rP1], a    ; Switch the key matrix
+    call .knownret  ; Burn 10 cycles calling a known ret
+    ldh a, [rP1]    ; Ignore value while waiting for the key matrix to settle
+    ldh a, [rP1]
+    ldh a, [rP1]    ; This read counts
+    or a, $F0       ; A7-4 = 1; A3-0 = unpressed keys
+
+.knownret
+    ret
+
+; =============================================================================
+SECTION "Sprites", ROM0
+
+Letters: INCBIN "text-font.2bpp"
 LettersEnd:
 
-Player:
-    dw `13333331
-    dw `30000003
-    dw `30000003
-    dw `30000003
-    dw `30000003
-    dw `30000003
-    dw `13333331
-    dw `00000000
-PlayerEnd:
+Spaceship: INCBIN "spaceship.2bpp"
+SpaceshipEnd:
+
+Background: INCBIN "background.2bpp"
+BackgroundEnd:
+
+BackgroundTilemap: INCBIN "background.tilemap"
+BackgroundTilemapEnd:
+
+; Work RAM, we can create variables here --------------------------------------
+SECTION "Input Variables", WRAM0
+wCurKeys: db
+wNewKeys: db
