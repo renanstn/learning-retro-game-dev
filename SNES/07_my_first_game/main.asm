9c53af845edc9432d4a3cb431ab107bee100c6e0
Renan <renan_ota@hotmail.com>
2026-05-28 23:30:32 -0300

    Update

--- SNES/07_my_first_game/main.asm
+++ SNES/07_my_first_game/main.asm
@@ -7,11 +7,6 @@
 .include "init.asm"
 
 ; -----------------------------------------------------------------------------
-.segment "ZEROPAGE"
-
-PlayerX:       .res 1
-PlayerY:       .res 1
-; -----------------------------------------------------------------------------
 .segment "CODE"
 
 InitPlayer:
@@ -153,12 +148,7 @@ Main:
     lda #1
     sta $420b           ; start transfer
 
-; Draw our character using metasprites ----------------------------------------
 	jsr InitPlayer
-	jsr DrawPlayer
-
-; DMA from OAM_BUFFER to the OAM RAM ------------------------------------------
-	jsr DMA_OAM
 
 ; Screen mode and other configs -----------------------------------------------
     lda #2              ;sprite tiles at $4000
@@ -175,13 +165,98 @@ Main:
     lda #(BG1_ON | SPR_ON) ; activate background and sprites
     sta TM
 
+    ; turn on NMI interrupts and auto-controller reads
+	lda #NMI_ON|AUTO_JOY_ON
+	sta NMITIMEN
+
 ; Turn the screen on (end forced blank) ---------------------------------------
 lda #FULL_BRIGHT    ; $0f - full brightness
 sta INIDISP         ; $2100 - effectively turns the screen ON
 
+; Game loop -------------------------------------------------------------------
 Infinite_Loop:
+    A8
+    XY16
+    jsr Wait_NMI
+    ; We are now in v-blank
+    jsr Pad_Poll
+    AXY16
+    ; Move player left
+    lda pad1
+    and #KEY_LEFT
+    beq @not_left
+@left:
+    A8
+    dec PlayerX
+    A16
+@not_left:
+    ; Move player right
+    lda pad1
+    and #KEY_RIGHT
+    beq @not_right
+@right:
+    A8
+    inc PlayerX
+    A16
+@not_right:
+
+    ; Render ------------------------------------------------------------------
+    jsr DrawPlayer
+    jsr DMA_OAM         ; DMA from OAM_BUFFER to the OAM RAM
+
     jmp Infinite_Loop
 
+; Functions -------------------------------------------------------------------
+Wait_NMI:
+    .a8
+    .i16
+    ;should work fine regardless of size of A
+	lda in_nmi      ;load A register with previous in_nmi
+    @check_again:
+	WAI             ;wait for an interrupt
+	cmp in_nmi	    ;compare A to current in_nmi
+				    ;wait for it to change
+					;make sure it was an nmi interrupt
+	beq @check_again
+	rts
+
+;------------------------------------------------------------------------------
+Pad_Poll:
+    .a8
+    .i16
+    ; reads both controllers to pad1, pad1_new, pad2, pad2_new
+    ; auto controller reads done, call this once per main loop
+    ; copies the current controller reads to these variables
+    ; pad1, pad1_new, pad2, pad2_new (all 16 bit)
+	php
+	A8
+@wait:          ; wait till auto-controller reads are done
+	lda HVBJOY  ; check HBlank, VBlank and JoypadBusy status
+	lsr a
+	bcs @wait
+
+	A16         ; put A to 16 bit, so we can read all buttons at once
+	; joypad 1
+	lda pad1
+	sta temp1   ; save last frame
+	lda JOY1L   ; $4218: controller 1 low
+	sta pad1
+	eor temp1
+	and pad1
+	sta pad1_new
+
+	; joypad 2
+	lda pad2
+	sta temp1   ; save last frame
+	lda JOY2L   ; $421a: controller 2 low
+	sta pad2
+	eor temp1
+	and pad2
+	sta pad2_new
+	plp
+	rts
+
+; -----------------------------------------------------------------------------
 .include "header.asm"
 .include "vectors.asm"
 
--- SNES/07_my_first_game/variables.asm
+++ SNES/07_my_first_game/variables.asm
@@ -1,12 +1,17 @@
-; variables declared
-
 .segment "ZEROPAGE"
 
-in_nmi: .res 2
+in_nmi:         .res 2
+temp1:          .res 2
+pad1:           .res 2
+pad1_new:       .res 2
+pad2:           .res 2
+pad2_new:       .res 2
+PlayerX:        .res 1
+PlayerY:        .res 1
 
+; -----------------------------------------------------------------------------
 .segment "BSS"
 
-PAL_BUFFER: .res 512 ; palette
-
-OAM_BUFFER: .res 512 ; low table
-OAM_BUFFER2: .res 32 ; high table
+PAL_BUFFER:     .res 512 ; palette
+OAM_BUFFER:     .res 512 ; low table
+OAM_BUFFER2:    .res 32  ; high table
