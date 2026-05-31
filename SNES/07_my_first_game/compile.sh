dd653a05f6677b8288e6e45e381d4b02b36b5ca8
renan <renan_ota@hotmail.com>
2026-05-31 19:57:25 -0300

    update

--- SNES/07_my_first_game/compile.sh
+++ SNES/07_my_first_game/compile.sh
@@ -2,4 +2,5 @@
 
 ../tools/cc65_bin/ca65 --cpu 65816 -o output.o main.asm && \
 ../tools/cc65_bin/ld65 -C smc.cfg output.o -o game.smc && \
-rm output.o
+rm output.o && \
+../tools/Snes9x-1.63-x86_64.AppImage game.smc
